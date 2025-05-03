c873710caf7a245aecb4d587eedfa5cd5a19d9c8
zh0rchik <georgiy.zyuzin73@mail.ru>
2025-05-03 17:09:54 +0400

    v-bind:...; @input=... -> v-model=... Заменил на v-model, тк более универсальное. Двустроннее связывание

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="app">
+    <post-form/>
+    <post-list :posts="posts" />  <!--передаем в компонент (бандим) как пропсы-->
+  </div>
+</template>
+
+<script>
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+// компонент, всё остальное - синтактический сахар
+export default {
+  //регистрация компонентов
+  components: {
+    PostForm, PostList
+  },
+  data(){
+    return{ // здесь модели
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods:{
+    createPost(){
+
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+
+      this.posts.push(newPost);
+
+      this.title = '';
+      this.body = '';
+    },
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app{
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+        v-model="post.title"
+        class="input"
+        type="text"
+        placeholder="Название">
+    <input
+        v-model="post.body"
+        class="input"
+        type="text"
+        placeholder="Описание">
+    <button
+        class="btn"
+    >
+      Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post:{
+        title:'',
+        body:'',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form{
+  display: flex;
+  flex-direction: column;
+}
+
+.btn{
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 15px 10px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+.input{
+  width: 100%;
+  border: 3px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+</style>
